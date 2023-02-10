257240f7cd676839c00e60d00021d309be4bf465
Valerie Kalchenko <valerie.kalchenko@gmail.com>
2023-02-09 21:40:43 -0500

    Febuary 9

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Amplify } from "aws-amplify";
 import awsconfig from './aws-exports';
 import { withAuthenticator } from "@aws-amplify/ui-react";
 import '@aws-amplify/ui-react/styles.css';
+import RestaurantContextProvider from "./context/RestaurantContext";
 
 Amplify.configure(awsconfig);
 
@@ -13,6 +14,7 @@ const { Sider, Content, Footer } = Layout;
 
 function App() {
   return (
+    <RestaurantContextProvider>
     <Layout>
       <Sider style={{backgroundColor: 'white'}}>
         <Image
@@ -30,6 +32,7 @@ function App() {
         </Footer>
       </Layout>
     </Layout>
+    </RestaurantContextProvider>
   );
 }
 
